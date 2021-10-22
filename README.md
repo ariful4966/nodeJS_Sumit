9203c6753bc4643197dc51e386d4a0fe502823cd
ariful4082 <ariful4082@gmail.com>
2021-10-22 11:40:59 +0600

    express stattic method use

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
+[![Facebook][facebook-shield]][https://www.facebook.com/md.ariful.islam4082]
+
 # Node JS Learning
 
 **Learn With Sumit**
 
 ### Express
-***Introduction***
+
+**_Introduction_**
+
+- [x] express
+- [x] express.json()
+- [x] express.raw();
+- [x] express.text();
+- [x] express.urlencoded();
+- [x] express.static();
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 
 const app = express();
-const port = 4000
+const port = 4000;
 
+app.use(
+    express.static(`${__dirname}/public/`, {
+        index: 'home.html',
+    })
+);
 
 app.get('/', (req, res) => {
-    res.send('This is home page')
-})
-app.post('/', (req, res)=>{
-    res.send('This is home page with post request')
-})
+    res.send('This is home page');
+});
+app.post('/', (req, res) => {
+    res.send('This is home page with post request');
+});
 
 app.listen(port, () => {
-    console.log('Listening on port ' + port);
-})
+    console.log(`Listening on port ${port}`);
+});
