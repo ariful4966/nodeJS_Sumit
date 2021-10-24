5b38b5d1bd361aa02056496daf308c4f019540ac
ariful4082 <ariful4082@gmail.com>
2021-10-25 00:09:26 +0600

    use request cookie and cookieparser

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express');
 
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const adminRoute = express.Router();
 const port = 4000;
+
+app.use(cookieParser());
+
 app.use('/admin', adminRoute);
+
 app.use(express.urlencoded({ extended: false }));
+
 app.use(express.json());
+
 adminRoute.get('/dashboard', (req, res) => {
     console.log(req.protocol);
     res.send('We are in admin Dashboard');
 });
 
 app.get('/user/:id', (req, res) => {
-    console.log(req.query);
+    console.log(req.cookies);
     res.send('Hello World');
 });
 
