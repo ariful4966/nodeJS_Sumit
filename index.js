9b9571f982e22bb5d3253fa0bdd088fd4d07ecc3
ariful4082 <ariful4082@gmail.com>
2021-10-26 00:52:44 +0600

    res.location and res.redirect

--- index.js
+++ index.js
@@ -5,27 +5,12 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.get('/test', (req, res) => {
+    res.send('Hello Mike testing');
+});
+
 app.get('/', (req, res) => {
-    // res.format({
-    //     'text/plain': () => {
-    //         res.send('hi');
-    //     },
-    //     'text/html': () => {
-    //         res.render('pages/about', {
-    //             name: 'Bangladesh',
-    //         });
-    //     },
-    //     'application/json': () => {
-    //         res.json({
-    //             message: 'About',
-    //         });
-    //     },
-    //     default: () => {
-    //         res.status(406).send('Not acceptable');
-    //     },
-    res.cookie('name', 'Hello Aysha Arif', {});
-    res.end();
-    // });
+    res.redirect('/test');
 });
 app.get('/about', (req, res) => {
     res.end('This is About Page');
