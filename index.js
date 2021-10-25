4bb6aba872ff7c317d5e476a8718f7711f111473
ariful4082 <ariful4082@gmail.com>
2021-10-26 00:46:14 +0600

    response cookie

--- index.js
+++ index.js
@@ -6,24 +6,26 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.format({
-        'text/plain': () => {
-            res.send('hi');
-        },
-        'text/html': () => {
-            res.render('pages/about', {
-                name: 'Bangladesh',
-            });
-        },
-        'application/json': () => {
-            res.json({
-                message: 'About',
-            });
-        },
-        default: () => {
-            res.status(406).send('Not acceptable');
-        },
-    });
+    // res.format({
+    //     'text/plain': () => {
+    //         res.send('hi');
+    //     },
+    //     'text/html': () => {
+    //         res.render('pages/about', {
+    //             name: 'Bangladesh',
+    //         });
+    //     },
+    //     'application/json': () => {
+    //         res.json({
+    //             message: 'About',
+    //         });
+    //     },
+    //     default: () => {
+    //         res.status(406).send('Not acceptable');
+    //     },
+    res.cookie('name', 'Hello Aysha Arif', {});
+    res.end();
+    // });
 });
 app.get('/about', (req, res) => {
     res.end('This is About Page');
