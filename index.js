c2c68c48064b1c763dc70ab1f340325bd3366ee5
ariful4082 <ariful4082@gmail.com>
2021-10-27 01:30:26 +0600

    application lavel middleware and router lavel middleware

--- index.js
+++ index.js
@@ -2,26 +2,27 @@ const express = require('express');
 
 const port = 4000;
 const app = express();
-
-const myMiddleware = (req, res, next) => {
-    console.log('I am logging');
+const adminRouter = express.Router();
+const logger = (req, res, next) => {
+    console.log(
+        `${new Date(Date.now()).toLocaleString()} - ${req.method}-${req.originalUrl}- ${
+            req.protocol
+        } - ${req.ip}`,
+    );
     next();
 };
+adminRouter.use(logger);
+app.use('/admin', adminRouter);
 
-const myMiddleware2 = (req, res, next) => {
-    console.log('I am logging 2');
-    next();
-};
-
-app.use(myMiddleware);
-
-app.use(myMiddleware2);
+adminRouter.get('/dashboard', (req, res) => {
+    res.send('Dashboard');
+});
 
 app.get('/', (req, res) => {
     res.send('This is home page');
 });
 
-app.get('/about', myMiddleware, myMiddleware2, (req, res) => {
+app.get('/about', (req, res) => {
     res.send('About page is hare');
 });
 
