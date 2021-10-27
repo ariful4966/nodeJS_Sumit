8c5cd3d4312bc1cf402bebc6888a90bca0b80c59
ariful4082 <ariful4082@gmail.com>
2021-10-27 22:27:49 +0600

    router param manage multiple function

--- publicRouter.js
+++ publicRouter.js
@@ -1,22 +1,16 @@
 const publicRouter = require('express').Router();
 
-// s
-
-// publicRouter.all('*', log);
-
-publicRouter.param('user', (req, res, next, id) => {
-    req.user = id === '1' ? 'Admin' : 'Anonymous';
-    console.log('I am called once');
-    next();
+publicRouter.param((param, option) => (req, res, next, val) => {
+    if (val === option) {
+        next();
+    } else {
+        res.sendStatus(403);
+    }
 });
-
-publicRouter.get('/:user', (req, res, next) => {
-    console.log('This also matches');
-    next();
-});
+publicRouter.param('user', '12');
 
 publicRouter.get('/:user', (req, res) => {
-    res.send(`Hello ${req.user}`);
+    res.send('Hello Admin');
 });
 publicRouter.get('/about', (req, res) => {
     res.send('About page is here');
