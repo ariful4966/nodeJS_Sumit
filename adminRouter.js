f81ab95bbec724066fe33ed4bda78c9c8216afcc
ariful4082 <ariful4082@gmail.com>
2021-10-29 00:30:18 +0600

    error handling

--- adminRouter.js
+++
@@ -1,9 +0,0 @@
-const adminRouter = require('express').Router();
-
-adminRouter.get('/', (req, res) => {
-    res.send('Dashboard');
-});
-adminRouter.get('/login', (req, res) => {
-    res.send('User Login Page');
-});
-module.exports = adminRouter;
--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 const express = require('express');
-const adminRouter = require('./adminRouter');
-const publicRouter = require('./publicRouter');
+const fs = require('fs');
 
 const app = express();
-const port = process.env.POST || 4000;
 
-app.use('/admin', adminRouter);
-app.use('/', publicRouter);
+app.get('/', [
+    (req, res, next) => {
+        fs.readFile('/file-doesnt-exist', 'urt-8', (err, data) => {
+            console.log(data);
+            next(err);
+        });
+    },
+    (req, res, next) => {
+        console.log(data.property);
+    },
+]);
+// setTimeout(() => {
+//     try {
+//         console.log(a);
+//     } catch (err) {
+//         next(err);
+//     }
+// });
+// fs.readFileSync('/file-does-not-exist', (err, data) => {
+//     if (err) {
+//         next(err);
+//     } else {
+//         res.send(data);
+//     }
+// });
+// });
+app.use((req, res, next) => {
+    console.log(' I am not called!');
+    next();
+});
 
-app.listen(port, () => {
-    console.log(`Listening on ${port}`);
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        next('There was a problem');
+    } else if (err.message) {
+        res.status(500).send(err.message);
+    } else {
+        res.send('There was an error!');
+    }
+});
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
 });
